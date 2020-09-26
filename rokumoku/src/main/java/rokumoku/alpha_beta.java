1158f53f87f2c618a9e8115b0874d1a79d1ea9c9
JWMON <jwmon@MacBook-Pro-2.local>
2020-09-26 18:45:10 +0900

    alpha beta

--- rokumoku/src/main/java/rokumoku/alpha_beta.java
+++ rokumoku/src/main/java/rokumoku/alpha_beta.java
@@ -1,5 +1,27 @@
 package rokumoku;
 
-public class alpha_beta {
+public class alpha_beta{
 
+	void alphabeta() { //position depth maxP
+		if(depth == 0 ) //or game over in position
+			//return position's weight
+			
+		if(maxP) {
+			int maxWeight = -99999;
+			for(Child child: position) {
+				weight = minimax(child, depth - 1, false);
+				maxWeight = max(maxWeight, weight);
+			}
+			return maxWeight;
+		}
+		else {
+			int minWeight = 99999;
+			for(Child child: position)
+			{
+				weight = minimax(child, depth - 1, true);
+				minWeight = min(minWeight, weight);
+			}
+			return minWeight;
+			
+	}
 }
